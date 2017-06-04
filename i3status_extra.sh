a6f1ac1a3baed7551bcb076e981505e0016aad34
axcxl <axcxl@users.noreply.github.com>
2017-06-04 16:39:43 +0300

    Added memory usage counter. Moved CPU first to be next to each other

---
+++ i3status_extra.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# shell script to prepend i3status with more stuff
+
+#i3status -c ~/.i3/i3status.conf | while :
+#do
+#        read line
+#        echo "mystuff | $line" || exit 1
+#done
+i3status -c ~/.i3/i3status.conf | (read line && echo "$line" && read line && echo "$line" && read line && echo "$line" && while :
+do
+  read line
+freemem=$( awk '/MemTotal/ {memtotal=$2}; /MemAvailable/ {memavail=$2}; END { printf("%.0f", (100- (memavail / memtotal * 100))) }' /proc/meminfo )
+  echo ",[{\"full_text\":\"MEM:${freemem}%\" },${line#,\[}" || exit 1
+done)
